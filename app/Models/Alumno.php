08bb5d866f7cc03d6185317ce32a7b22d32a6118
Aarón Velasco <aaronvdlp@gmail.com>
2021-01-16 17:45:30 +0100

    fix: :card_file_box: Correcciones modelo Alumno

--- app/Models/Alumno.php
+++ app/Models/Alumno.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Alumno extends Model
 {
-    protected $fillable = ['nombre','apellidos','direccion','poblacion','codigo_postal','curso'];
+    use HasFactory;
+
+    protected $primaryKey = 'uuid';
+
+    protected $fillable = ['uuid','nombre','apellidos','direccion','poblacion','codigo_postal','curso'];
 }
